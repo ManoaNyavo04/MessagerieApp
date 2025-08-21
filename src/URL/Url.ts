a8e79b8896f73b8594f7625a9383e9a92f6c955c
Manoa <manoanyavoampoina@gmail.com>
2025-08-21 08:53:18 +0300

    fix : configurer url prod

--- src/URL/Url.ts
+++ src/URL/Url.ts
@@ -1,6 +1,6 @@
-export const baseUrl = "http://localhost:5032"
+export const baseUrl = process.env.REACT_APP_API_URL || "http://localhost:5032";
 
-export const LOGIN_URL = `${baseUrl}/api/Utilisateur`
-export const ALL_USER_URL = `${baseUrl}/api/Utilisateur`
-export const USER_DISCUSSION = `${baseUrl}/api/Message`
-export const ROLE_URL = `${baseUrl}/api/Role`
+export const LOGIN_URL = `${baseUrl}/api/Utilisateur`;
+export const ALL_USER_URL = `${baseUrl}/api/Utilisateur`;
+export const USER_DISCUSSION = `${baseUrl}/api/Message`;
+export const ROLE_URL = `${baseUrl}/api/Role`;
