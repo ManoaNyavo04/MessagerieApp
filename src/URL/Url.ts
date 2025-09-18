0a6c15cbe3180807c7db934d5e54764f4118cf4d
Manoa <manoanyavoampoina@gmail.com>
2025-09-18 10:41:05 +0300

    fix : basculer backend prod en http

--- src/URL/Url.ts
+++ src/URL/Url.ts
@@ -1,6 +1,6 @@
 export const PROD = true;
 
-export const baseUrl = PROD ? "http://10.5.100.7:5031" : "http://localhost:5032";
+export const baseUrl = PROD ? "https://10.5.100.7:5031" : "http://localhost:5032";
 // export const baseUrl = PROD ? "http://localhost:5032" : "http://localhost:5032";
 
 export const LOGIN_URL = `${baseUrl}/api/Utilisateur`;
